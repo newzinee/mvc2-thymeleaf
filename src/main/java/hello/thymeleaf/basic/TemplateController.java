a12fdb32baa1f99cd61036682a296176f36fe716
treasureBear94 <qvo7896@gmail.com>
2021-08-14 23:05:33 +0900

    :sparkles: layout

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -1,5 +1,6 @@
 package hello.thymeleaf.basic;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,12 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @version 0.1.0
  * @since 2021/08/08
  */
+@Controller
 @RequestMapping("/template")
 public class TemplateController {
 
-
     @GetMapping("/fragment")
-    public String tempalate() {
+    public String template() {
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
 }
